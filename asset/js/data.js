335be76446df5f7da192b6c048f8fa353a8ac375
Mohamilr <ibrahimdamy7@gmail.com>
2021-07-04 23:06:10 +0100

    finish up functionality

---
+++ asset/js/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972269/product3_iyadfq.png",
+        name: "PS 5",
+        price: "400000"
+    },
+    {
+        id: 2,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972269/product5_mothwj.png",
+        name: "watch",
+        price: "20000"
+    },
+    {
+        id: 3,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972269/product2_j1wvfr.png",
+        name: "Techno spark 4",
+        price: "30000"
+    },
+    {
+        id: 4,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972269/product4_pwfxla.png",
+        name: "hp 640",
+        price: "80000"
+    },
+    {
+        id: 5,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972268/product1_ktbggp.png",
+        name: "television",
+        price: "40000"
+    },
+    {
+        id: 6,
+        image: "https://res.cloudinary.com/project-s/image/upload/v1619972268/product6_epx7oo.png",
+        name: "phone accessories",
+        price: "2000"
+    }
+]
